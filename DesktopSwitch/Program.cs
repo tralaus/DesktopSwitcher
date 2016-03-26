5efc7ff5be42b7791dcf4e944c62c10deefc1646
Shahar Ben David <tralaus@gmail.com>
2016-03-26 19:56:53 +0300

    First version

---
+++ DesktopSwitch/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
