fb136edf555ac7d2fa823c475f09f3927a7917c5
Shahar Ben David <tralaus@gmail.com>
2016-03-26 20:24:20 +0300

    Cleanup

--- DesktopSwitcher/Properties/AssemblyInfo.cs
+++ DesktopSwitcher/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSRegisterHotkey")]
+[assembly: AssemblyTitle("DesktopSwitcher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CSRegisterHotkey")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DesktopSwitcher")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
